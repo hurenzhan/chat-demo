3b50996fb2de0c0cf2e43b2c74d1eb48fe38acc1
hurenren <hurenfa@gmail.com>
2023-09-18 08:42:19 +0800

    feat: 初始化

---
+++ prompt/01.py
@@ -0,0 +1,22 @@
+import os  # 用于读取环境变量
+import openai  # OpenAI 的 Python SDK，用于调用 API
+from dotenv import load_dotenv, find_dotenv  # 用于读取 .env 文件
+
+_ = load_dotenv(find_dotenv())  # 加载 .env 到环境变量
+
+# 配置 OpenAI 服务
+openai.api_key = os.getenv('OPENAI_API_KEY')  # 设置 OpenAI 的 key
+openai.api_base = os.getenv('OPENAI_API_BASE')  # 指定代理地址
+
+# 调用 OpenAI API
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "user",
+            "content": "你的模型是基于GPT-3还是GPT-3.5"
+        }
+    ],
+)
+
+print(response.choices[0].message["content"])
